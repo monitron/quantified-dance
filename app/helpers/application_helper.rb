9f47503613e9ced5009978511824293b46afe900
Justin Streufert <monitron@gmail.com>
2014-03-13 04:55:49 -0400

    Display profiles list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def relative_date(date)
+    diff = (Date.today - date).to_i
+    if diff == 0
+      "today"
+    elsif diff == -1
+      "yesterday"
+    elsif diff == 1
+      "tomorrow"
+    elsif diff < 0
+      "#{-diff} days ago"
+    else
+      "#{diff} days from now"
+    end
+  end
 end
