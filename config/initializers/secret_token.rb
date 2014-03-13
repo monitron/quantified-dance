e9a2ee1279d571dd201c1599df3bd45dd89b0a9c
Justin Streufert <monitron@gmail.com>
2014-03-13 01:50:43 -0400

    Init Rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuantifiedDance::Application.config.secret_key_base = 'abf24956ea4db004c5fd884e57a3706d83b7fc731f92f47366e2a0bb7a915a07ad08762570f7134719099989145d71afd36a7de162ada3429e48d5e119b70653'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuantifiedDance::Application.config.session_store :cookie_store, key: '_quantified-dance_session'
