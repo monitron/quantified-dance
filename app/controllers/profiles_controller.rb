adc78da7d755ffcbef47bad2298a66ba4aec0c51
Justin Streufert <monitron@gmail.com>
2014-03-15 02:04:51 -0400

    Profile page - Favorite Songs

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,5 +8,9 @@ class ProfilesController < ApplicationController
     @recent_days = @profile.plays.
       select("date(played_at, 'localtime') as date, count(1) as num, sum(survive_seconds) as secs").
       group('date').order('date desc').limit(10)
+    # 1+n but .include() kills the count :( at least n <= 10
+    @favorite_songs = @profile.plays.joins(:chart).
+      select('count(1) as num_plays, chart_id').
+      group('charts.id').order('num_plays desc').limit(5)
   end
 end
